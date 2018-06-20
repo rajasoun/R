a8e2ab487dc299af3b769d86ae60fa338e3e3041
Raja. Soundaramourty <rajasoun@icloud.com>
2018-06-20 19:21:57 +0530

    Add Features For Column Data Analysis

--- data.table_spike.R
+++ data.table_spike.R
@@ -1,3 +1,5 @@
+#!/usr/bin/env Rscript
+
 # Packages Used
 # pryr - R Internals
 # data.table - Reading Big Data
@@ -19,9 +21,9 @@ for (pkg in pkgs) {
 lapply(pkgs, library, character.only = TRUE)
 
 # Set Working Directory For Data
-# setwd("~/Workspace/prototypes/data-science/data")
 getwd()
-setwd("~/workspace/data-science/data")
+setwd("~/Workspace/prototypes/data-science/data")
+# setwd("~/workspace/data-science/data")
 challenge_data <- "challengeData.tsv"
 data_file <- "training_set.tsv"
 
@@ -39,14 +41,42 @@ dataset <- fread(challenge_data,
 toc()
 dim(dataset)
 
-# get first 2 rows of the Data Frame
+# Tidy the Data [Meaningfull Column Names]
+names(dataset) %<>%
+  toupper() %>%
+  str_replace_all("IBSA_MAIN_SMALL_TRAINING_SET.", "")
+
+# Understand The Data
+colnames(dataset) # Names of teh Columns
+glimpse(dataset)
+
+# Extract Columns Names based on Name Patterns
+column_names <- names(dataset)
+column_names
+
+# Get Column Number given a Column Name
+column_name <- "INSTALLATION_DATE"
+column_number <- which( colnames(dataset) == column_name )
+column_number
+
+
+
+# Column Names Containing DATE
+column_with_id <- column_names[grep("DATE",column.names,fixed=TRUE)]
+column_with_id
+
+# Get first 2 rows of the Data Frame
 ans <- dataset[1:2]
 head(ans)
 
-ans <- dataset[,ibsa_main_small_training_set.instance_status]
-ans
-
+# Get Unique Values for each Column in the Data Frame
 for (i in 1:ncol(dataset)) {
   unique_col_values <- unique(dataset[, i, with=FALSE])
   print(unique_col_values)
 }
+
+
+unique(dataset[ , which( colnames(dataset) == "INSTALLATION_DATE" ), with = FALSE])
+
+ans <- dataset[RENEWED_YORN == "Y" & INSTALLATION_DATE == 6L]
+
